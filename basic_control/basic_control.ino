ecf3bd88965648b2a3f385dff3064cb8b8d1949f
Simrun Mutha <70967428+simrunm@users.noreply.github.com>
2021-10-20 23:06:07 -0400

    Adding comments

--- basic_control/basic_control.ino
+++ basic_control/basic_control.ino
@@ -4,20 +4,20 @@ Adafruit_DCMotor *leftMotor = AFMS.getMotor(2);
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
 
 // Setup function to initialize hardware and software
-const int leftSensor = A0;
-const int rightSensor = A1;// analog pin used to connect the sharp sensor
-int sensorLeft = 0;// variable to store the values from s
-int sensorRight = 0;
-int leftMotorSpeed = 0;
-int rightMotorSpeed = 0;
-int thresholdTape = 150;
-const int fastSpeed = 25;
-int right_deg = 0;
-int left_deg = 0;
-int loop_count = 0;
-const int mod_value = 20;
-int isReady = 0;
-char receivedChar;
+const int leftSensor = A0; // analog pin used to connecting to the left sensor
+const int rightSensor = A1; // analog pin used to connecting to the right sensor
+int sensorLeft = 0; // storing value from left sensor
+int sensorRight = 0; // storing value from right sensor
+int leftMotorSpeed = 0; // left motor speed
+int rightMotorSpeed = 0; // right motor speed
+int thresholdTape = 150; // If the sensor reading is higher than the threshold, then its on the sensor
+const int fastSpeed = 25; // fast motor speed
+int right_deg = 0; // variable for controlling steepness of right turns
+int left_deg = 0; // variable for controlling steepness of left turns
+int loop_count = 0; // loop iteration
+const int mod_value = 20; // value determining how often the turn changes
+int isReady = 0; // 0 or 1 to determine if the robot is not ready or ready
+char receivedChar; // value received from python
 boolean newData = false;
 
 void setup(){
@@ -73,9 +73,11 @@ void stopMotors(){
 void loop() 
 {
   recvOneChar();
+  // if R is received from the serial moniter, then the robot will run
   if (receivedChar == 'R'){ 
     isReady = 1;
   }
+  // if S is received from the serial moniter, then the robot will stop
   if (receivedChar == 'S'){
     isReady = 0;
     stopMotors();
@@ -83,9 +85,9 @@ void loop()
   if (isReady){
     sensorLeft = analogRead(leftSensor); // reading the value from the sensor
     sensorRight = analogRead(rightSensor); // reading the value from the sensor
-    Serial.print(sensorLeft); // printing the value to serial prot
+    Serial.print(sensorLeft); // printing the value to serial port
     Serial.print(" "); 
-    Serial.println(sensorRight); // printing the value to serial prot
+    Serial.println(sensorRight); // printing the value to serial port
      
     loop_count += 1;
     // LEFT TURN: Right sensor on tape, left sensor not
@@ -98,12 +100,12 @@ void loop()
       leftTurn(left_deg);
     }
     // STRAIGHT
+    // both sensors are on the tape
     else if (sensorLeft >= thresholdTape && sensorRight >= thresholdTape){
       Serial.println("going straight");
       right_deg = 0;
       left_deg = 0;
       goStraight();
-  //    stopMotors();
      }
      // RIGHT TURN: Left sensor on tape, right sensor not
      else if (sensorRight >= thresholdTape && sensorLeft <= thresholdTape){
